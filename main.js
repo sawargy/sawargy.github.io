b0964cdf9a4824e24aba1397024f6c2c4fb5af65
abullaisi <iabullaisi@gmail.com>
2023-04-17 14:54:42 +0700

    minor

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Impact - v1.2.0
-* Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
